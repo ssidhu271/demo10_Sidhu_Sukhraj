5ed0488fa2c33babcd8e70183f3e74d160e0fca3
ssidhu271 <ssidhu271@my.bcit.ca>
2022-11-15 10:38:45 -0800

    Added review hikes, and some .js edits.

--- scripts/main.js
+++ scripts/main.js
@@ -29,3 +29,28 @@ function insertName(){
 }
 insertName();
 
+function populateCardsDynamically() {
+    let hikeCardTemplate = document.getElementById("hikeCardTemplate");
+    let hikeCardGroup = document.getElementById("hikeCardGroup");
+    
+    db.collection("hikes").get()
+        .then(allHikes => {
+            allHikes.forEach(doc => {
+                var hikeName = doc.data().name; //gets the name field
+                var hikeID = doc.data().code; //gets the unique ID field
+                var hikeLength = doc.data().length; //gets the length field
+                let testHikeCard = hikeCardTemplate.content.cloneNode(true);
+                testHikeCard.querySelector('.card-title').innerHTML = hikeName;     //equiv getElementByClassName
+                testHikeCard.querySelector('.card-length').innerHTML = hikeLength;  //equiv getElementByClassName
+                testHikeCard.querySelector('a').onclick = () => setHikeData(hikeID);//equiv getElementByTagName
+                testHikeCard.querySelector('img').src = `./images/${hikeID}.jpg`;   //equiv getElementByTagName
+                hikeCardGroup.appendChild(testHikeCard);
+            })
+
+        })
+}
+populateCardsDynamically();
+
+function setHikeData(id){
+    localStorage.setItem ('hikeID', id);
+}
